bf749bd3d2e06166f0782cf5d36fe1caccd2faa0
zhiheng.jiang <zhiheng.jiang@ttpai.cn>
2019-05-10 12:21:00 +0800

    代码文件位置调整

--- examples/keyDown.ts
+++ examples/keyDown.ts
@@ -1,5 +1,4 @@
 function listenKeyDown() {
-    console.log(window.innerHeight+","+window.innerWidth);
     window.addEventListener('keydown',function (event) {
         if(event.key==='Backspace'){
             window.history.back();
