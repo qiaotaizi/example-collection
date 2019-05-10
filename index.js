65a0ada33423624791b67bf43be0785d57b2f350
zhiheng.jiang <zhiheng.jiang@ttpai.cn>
2019-05-10 10:02:13 +0800

    搭建项目

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+const { app, BrowserWindow } = require("electron");
+let win;
+function createWindow() {
+    win = new BrowserWindow({
+        title: "three例子集",
+        show: false,
+        webPreferences: {
+            nodeIntegration: true //这个特性开启之后,渲染进程才能够正常调用require方法获取模块
+        },
+    });
+    win.maximize();
+    win.loadFile("app.html");
+    win.once();
+    //打开开发者工具
+    win.webContents.openDevTools();
+    win.on("closed", () => {
+        win = null;
+    });
+}
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+app.on("activate", () => {
+    if (win === null) {
+        createWindow();
+    }
+});
+//# sourceMappingURL=index.js.map
---
+++ render/render.ts
@@ -0,0 +1,11 @@
+function writeContent() {
+
+    let span=document.getElementById("content-span");
+    if(span!==null){
+        span.innerText=
+            `We are using node ${process.versions.node},
+            V8 ${process.versions.v8},`;
+    }
+}
+
+writeContent();
