cd239b0fe279a0efe358a371e3f5f544cdb73187
zhiheng.jiang <zhiheng.jiang@ttpai.cn>
2019-05-13 18:07:01 +0800

    绑定多动画状态

--- examples/webgl-animation-skinning-morph/ts/skinningMorph.js
+++ examples/webgl-animation-skinning-morph/ts/skinningMorph.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const three_1 = require("three");
+const initGLTFLoader = require("../../initGLTFLoader");
 let camera, scene, clock, mixer;
 let renderer;
+let model;
+let activateAction, previousAction;
+let animations;
 init();
 eventBinding();
 animate();
@@ -20,7 +24,10 @@ function init() {
     let light = new three_1.DirectionalLight(0xffffff);
     light.position.set(0, 20, 10);
     scene.add(light);
-    let mesh = new three_1.Mesh(new three_1.PlaneBufferGeometry(2000, 2000), new three_1.MeshPhongMaterial({ color: 0x999999, depthWrite: false }));
+    let mesh = new three_1.Mesh(new three_1.PlaneBufferGeometry(2000, 2000), new three_1.MeshPhongMaterial({
+        color: 0x999999,
+        depthWrite: false
+    }));
     mesh.rotation.x = -Math.PI * 0.5;
     scene.add(mesh);
     let grid = new three_1.GridHelper(200, 40, 0x000000, 0x000000);
@@ -31,6 +38,34 @@ function init() {
     }
     scene.add(grid);
     //model
+    let loader = initGLTFLoader();
+    loader.load("../assets/RobotExpressive.glb", function (gltf) {
+        model = gltf.scene;
+        mixer = new three_1.AnimationMixer(model);
+        //打印所有动画片段
+        /**
+         * 0 : Dance
+         * 1 : Death
+         * 2 : Idle
+         * 3 : Jump
+         * 4 : No
+         * 5 : Punch
+         * 6 : Running
+         * 7 : Sitting
+         * 8 : Standing
+         * 9 : ThumbsUp
+         * 10 : Walking
+         * 11 : WalkJump
+         * 12 : Wave
+         * 13 : Yes
+         */
+        animations = gltf.animations;
+        activateAction = mixer.clipAction(animations[2]); //以idle状态初始化
+        activateAction.play();
+        scene.add(model);
+    }, undefined, function (err) {
+        console.error(err);
+    });
     renderer = new three_1.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -39,23 +74,58 @@ function init() {
     container.appendChild(renderer.domElement);
 }
 function animate() {
+    let dt = clock.getDelta();
+    if (mixer) {
+        mixer.update(dt);
+    }
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
+function fadeToAction(action, duration) {
+    previousAction = activateAction;
+    activateAction = action;
+    if (previousAction !== activateAction) {
+        previousAction.fadeOut(duration);
+        activateAction.reset().setEffectiveTimeScale(1).setEffectiveWeight(1).fadeIn(duration).play();
+    }
+}
+/**
+ * 绑定控制按钮点击事件
+ */
 function eventBinding() {
-    console.log("1");
-    let btn1 = document.getElementById("test1_btn");
-    if (btn1) {
-        console.log("2");
-        btn1.addEventListener("click", function (event) {
-            console.log("test1 clicked");
+    let idleBtn = document.getElementById("idle_btn");
+    if (idleBtn) {
+        idleBtn.addEventListener("click", function (event) {
+            console.log("进入待机状态");
+            fadeToAction(mixer.clipAction(animations[2]), 0.5);
         });
     }
-    let btn2 = document.getElementById("test2_btn");
-    if (btn2) {
-        console.log("3");
-        btn2.addEventListener("click", function (event) {
-            console.log("test2 clicked");
+    let walkBtn = document.getElementById("walk_btn");
+    if (walkBtn) {
+        walkBtn.addEventListener("click", function (event) {
+            console.log("进入行走状态");
+            fadeToAction(mixer.clipAction(animations[10]), 0.5);
+        });
+    }
+    let angryRange = document.getElementById("angry_range");
+    if (angryRange) {
+        angryRange.addEventListener("change", function (event) {
+            let input = this;
+            console.log("愤怒=" + input.value);
+        });
+    }
+    let sadRange = document.getElementById("sad_range");
+    if (sadRange) {
+        sadRange.addEventListener("change", function (event) {
+            let input = this;
+            console.log("悲伤=" + input.value);
+        });
+    }
+    let surpriseRange = document.getElementById("surprise_range");
+    if (surpriseRange) {
+        surpriseRange.addEventListener("change", function (event) {
+            let input = this;
+            console.log("惊讶=" + input.value);
         });
     }
 }
--- examples/webgl-animation-skinning-morph/ts/skinningMorph.ts
+++ examples/webgl-animation-skinning-morph/ts/skinningMorph.ts
@@ -1,4 +1,5 @@
 import {
+    AnimationAction, AnimationClip,
     AnimationMixer,
     Clock,
     Color,
@@ -9,10 +10,18 @@ import {
     Vector3,
     WebGLRenderer
 } from "three";
+import initGLTFLoader = require("../../initGLTFLoader");
+import {GLTF} from "three/examples/jsm/loaders/GLTFLoader";
 
-let camera:PerspectiveCamera,scene:Scene,clock:Clock,mixer:AnimationMixer;
+let camera: PerspectiveCamera, scene: Scene, clock: Clock, mixer: AnimationMixer;
 
-let renderer:WebGLRenderer;
+let renderer: WebGLRenderer;
+
+let model: Scene;
+
+let activateAction: AnimationAction, previousAction: AnimationAction;
+
+let animations:AnimationClip[];
 
 init();
 
@@ -22,67 +31,153 @@ animate();
 
 function init() {
 
-    let container=document.createElement("div");
-    container.id="container";
+    let container = document.createElement("div");
+    container.id = "container";
     document.body.appendChild(container);
-    camera=new PerspectiveCamera(45,innerWidth/innerHeight,0.25,100);
-    camera.position.set(-5,3,10);
-    camera.lookAt(new Vector3(0,2,0));
+    camera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.25, 100);
+    camera.position.set(-5, 3, 10);
+    camera.lookAt(new Vector3(0, 2, 0));
 
-    scene=new Scene();
-    scene.background=new Color(0xe0e0e0);
-    scene.fog=new Fog(0xe0e0e0,20,100);
+    scene = new Scene();
+    scene.background = new Color(0xe0e0e0);
+    scene.fog = new Fog(0xe0e0e0, 20, 100);
 
-    clock=new Clock();
+    clock = new Clock();
 
-    let light=new DirectionalLight(0xffffff);
-    light.position.set(0,20,10);
+    let light = new DirectionalLight(0xffffff);
+    light.position.set(0, 20, 10);
     scene.add(light);
 
-    let mesh=new Mesh(new PlaneBufferGeometry(2000,2000),new MeshPhongMaterial({color:0x999999,depthWrite:false}));
-    mesh.rotation.x=-Math.PI*0.5;
+    let mesh = new Mesh(new PlaneBufferGeometry(2000, 2000), new MeshPhongMaterial({
+        color: 0x999999,
+        depthWrite: false
+    }));
+    mesh.rotation.x = -Math.PI * 0.5;
     scene.add(mesh);
 
-    let grid=new GridHelper(200,40,0x000000,0x000000);
-    let gridMat=grid.material;
-    if(gridMat instanceof Material){
-        gridMat.opacity=0.2;
-        gridMat.transparent=true;
+    let grid = new GridHelper(200, 40, 0x000000, 0x000000);
+    let gridMat = grid.material;
+    if (gridMat instanceof Material) {
+        gridMat.opacity = 0.2;
+        gridMat.transparent = true;
     }
     scene.add(grid);
 
     //model
+    let loader = initGLTFLoader();
+    loader.load("../assets/RobotExpressive.glb", function (gltf: GLTF) {
+        model = gltf.scene;
+        mixer = new AnimationMixer(model);
+        //打印所有动画片段
+        /**
+         * 0 : Dance
+         * 1 : Death
+         * 2 : Idle
+         * 3 : Jump
+         * 4 : No
+         * 5 : Punch
+         * 6 : Running
+         * 7 : Sitting
+         * 8 : Standing
+         * 9 : ThumbsUp
+         * 10 : Walking
+         * 11 : WalkJump
+         * 12 : Wave
+         * 13 : Yes
+         */
+        animations=gltf.animations;
+        activateAction = mixer.clipAction(animations[2]);//以idle状态初始化
+        activateAction.play();
+
+        scene.add(model);
+    }, undefined, function (err: ErrorEvent) {
+        console.error(err);
+    });
 
 
-
-    renderer=new WebGLRenderer({antialias:true});
+    renderer = new WebGLRenderer({antialias: true});
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth,window.innerHeight);
-    renderer.gammaOutput=true;
-    renderer.gammaFactor=2.2;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.gammaOutput = true;
+    renderer.gammaFactor = 2.2;
     container.appendChild(renderer.domElement)
 }
 
 function animate() {
+    let dt = clock.getDelta();
+    if (mixer) {
+        mixer.update(dt);
+    }
     requestAnimationFrame(animate);
-    renderer.render(scene,camera);
+    renderer.render(scene, camera);
 }
 
+function fadeToAction(action: AnimationAction, duration: number) {
+    previousAction = activateAction;
+    activateAction = action;
+    if (previousAction !== activateAction) {
+        previousAction.fadeOut(duration);
+        activateAction.reset().setEffectiveTimeScale(1).setEffectiveWeight(1).fadeIn(duration).play();
+    }
+}
+
+/**
+ * 绑定控制按钮点击事件
+ */
 function eventBinding() {
-    console.log("1");
-    let btn1=document.getElementById("test1_btn");
-    if(btn1){
-        console.log("2");
-        btn1.addEventListener("click",function (event) {
-            console.log("test1 clicked");
+    let idleBtn = document.getElementById("idle_btn");
+    if (idleBtn) {
+        idleBtn.addEventListener("click", function (event) {
+            console.log("进入待机状态");
+            fadeToAction(mixer.clipAction(animations[2]),0.5);
         })
     }
 
-    let btn2=document.getElementById("test2_btn");
-    if(btn2){
-        console.log("3");
-        btn2.addEventListener("click",function (event) {
-            console.log("test2 clicked");
+    let walkBtn = document.getElementById("walk_btn");
+    if (walkBtn) {
+        walkBtn.addEventListener("click", function (event) {
+            console.log("进入行走状态");
+            fadeToAction(mixer.clipAction(animations[10]),0.5);
+        })
+    }
+
+    let danceBtn = document.getElementById("dance_btn");
+    if (danceBtn) {
+        danceBtn.addEventListener("click", function (event) {
+            console.log("进入跳舞状态");
+            fadeToAction(mixer.clipAction(animations[0]),0.5);
+        })
+    }
+
+    let runBtn = document.getElementById("run_btn");
+    if (runBtn) {
+        runBtn.addEventListener("click", function (event) {
+            console.log("进入奔跑状态");
+            fadeToAction(mixer.clipAction(animations[6]),0.5);
+        })
+    }
+
+    let angryRange = document.getElementById("angry_range");
+    if (angryRange) {
+        angryRange.addEventListener("change", function (this, event) {
+            let input = this as HTMLInputElement;
+            console.log("愤怒=" + input.value);
+        })
+    }
+
+    let sadRange = document.getElementById("sad_range");
+    if (sadRange) {
+        sadRange.addEventListener("change", function (this, event) {
+            let input = this as HTMLInputElement;
+            console.log("悲伤=" + input.value);
+        })
+    }
+
+    let surpriseRange = document.getElementById("surprise_range");
+    if (surpriseRange) {
+        surpriseRange.addEventListener("change", function (this, event) {
+            let input = this as HTMLInputElement;
+            console.log("惊讶=" + input.value);
         })
     }
 }
