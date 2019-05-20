688a5d5350d5574df29b4461503476963f9ae255
zhiheng.jiang <zhiheng.jiang@ttpai.cn>
2019-05-20 17:52:45 +0800

    趁机敲一会

--- examples/camera-cinematic/ts/cameraCinematic.ts
+++ examples/camera-cinematic/ts/cameraCinematic.ts
@@ -2,14 +2,18 @@ import {
     AmbientLight,
     BoxBufferGeometry,
     Color, ColorKeywords,
-    DirectionalLight, Mesh, MeshLambertMaterial,
+    DirectionalLight, Mesh, MeshLambertMaterial, Object3D,
     PerspectiveCamera,
     Raycaster,
-    Scene, WebGLRenderer
+    Scene, Vector2, WebGLRenderer
 } from "three";
-import red = ColorKeywords.red;
+import {degToRad} from "../../utils";
 
 let camera:PerspectiveCamera,scene:Scene,raycaster:Raycaster,renderer:WebGLRenderer;
+
+let mouse:Vector2=new Vector2(),oldColor:number;
+
+let theta:number=0,radius=100;
 
 init();
 
@@ -44,16 +48,55 @@ function init() {
         scene.add(obj);
     }
 
+    raycaster=new Raycaster();
 
     renderer=new WebGLRenderer({antialias:true});
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth,window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+    document.addEventListener('mousemove',function (event) {
+        event.preventDefault();
+        mouse.x=(event.clientX/window.innerWidth)*2-1;
+        mouse.y=(event.clientY/window.innerHeight)*2+1;
+    },false)
+
 }
 
 function animate() {
     requestAnimationFrame(animate);
 
+    render();
+}
+
+function cameraFocusAt(targetDistance: number) {
+
+}
+
+function render() {
+    theta+=0.1;
+
+    let xyRotation=Math.sin(degToRad(theta));
+    let zRotation=Math.cos(degToRad(theta));
+    camera.position.x=radius*xyRotation;
+    camera.position.y=radius*xyRotation;
+    camera.position.z=radius*zRotation;
+
+    camera.lookAt(scene.position);
+
+    camera.updateMatrixWorld(false);
+
+    raycaster.setFromCamera(mouse,camera);
+
+    let intersects=raycaster.intersectObjects(scene.children);
+
+    if(intersects.length>0){
+        let targetDistance=intersects[0].distance;
+        cameraFocusAt(targetDistance);
+    }
+
+
+
     renderer.render(scene,camera);
+
 }
--- examples/utils.ts
+++ examples/utils.ts
@@ -3,7 +3,18 @@
  * 该方法导入存在问题
  * @param range
  */
-export function randFloatSpread2(range:number):number {
-        return range * ( 0.5 - Math.random() );
+export function randFloatSpread2(range: number): number {
+    return range * (0.5 - Math.random());
 }
 
+export function degToRad(degrees: number) {
+    return degrees * DEG2RAD;
+}
+
+export function radToDeg(radians: number) {
+    return radians * RAD2DEG;
+}
+
+
+const DEG2RAD = Math.PI / 180;
+const RAD2DEG = 180 / Math.PI;
